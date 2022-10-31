modified:   train.py fixed training error calculation

train.py
@@ -53,7 +53,7 @@ def train(net, batch_size=32, lr=0.001, num_epochs=30):
 
             pred = softmax(outputs)
             # find error and loss for training data
-            total_val_err += (np.argmax(pred.detach().cpu(), 1) != np.argmax(labels.cpu(), 1)).sum().item()
+            total_train_err += (np.argmax(pred.detach().cpu(), 1) != np.argmax(labels.cpu(), 1)).sum().item()
             total_train_loss += loss.item()
             train_iters += len(labels)
 