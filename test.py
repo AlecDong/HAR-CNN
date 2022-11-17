d2e8104269212a30ba36d5567d4b7d219d816c64
AlecDong <101013007+AlecDong@users.noreply.github.com>
2022-11-17 16:14:02 -0500

    modified:   Human Action Recognition/Testing_set.csv 	modified:   test.py

--- test.py
+++ test.py
@@ -44,6 +44,47 @@ def show_test_images(n):
             break
     plt.show()
 
+def count_classes():
+    classes = {
+        0:"sitting",
+        1:"using_laptop",
+        2:"hugging",
+        3:"sleeping",
+        4:"drinking",
+        5:"clapping",
+        6:"dancing",
+        7:"cycling",
+        8:"calling",
+        9:"laughing",
+        10:"eating",
+        11:"fighting",
+        12:"listening_to_music",
+        13:"running",
+        14:"texting"
+    }
+    counts = {
+        "sitting":0,
+        "using_laptop":0,
+        "hugging":0,
+        "sleeping":0,
+        "drinking":0,
+        "clapping":0,
+        "dancing":0,
+        "cycling":0,
+        "calling":0,
+        "laughing":0,
+        "eating":0,
+        "fighting":0,
+        "listening_to_music":0,
+        "running":0,
+        "texting":0
+    }
+    test_loader = test_data_loader()
+    for batch in test_loader:
+        for _, img_class in zip(batch["image"], batch["img_class"]):
+            counts[classes[np.argmax(img_class).item()]] += 1
+    return counts
+
 def get_test_acc(net):
     if torch.cuda.is_available():
         net.cuda()
@@ -66,3 +107,10 @@ def get_test_acc(net):
             pred = softmax(avg_output)
             total_test_acc += (np.argmax(pred.detach().cpu(), 1) == np.argmax(img_class.unsqueeze(0).cpu(), 1)).sum().item()
     return total_test_acc / num_images
+
+if __name__=="__main__":
+    from model import CNN
+    net = CNN()
+    net.load_state_dict(torch.load("./models/bs256_lr0.0001_epoch29", map_location=torch.device('cpu')))
+    print(get_test_acc(net))
+    print(count_classes())
